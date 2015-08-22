1684db0f9cfd34c715a10bfc80d78d18eaafcfbc
arpith20 <arpith@live.com>
2015-08-22 22:58:55 +0530

    Added script to measure the time required to run a program

--- performance.sh
+++ performance.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 cd ./src/threads/
-echo "cleaning..."
-make clean
-echo "building"
-make
-echo "running alarm-signal"
-cd build
-pintos --qemu -- -q run alarm-single
-START=$SECONDS
-make check
-END=$SECONDS
-TIME=`expr $END - $START`
-echo "it took "$TIME "seconds to run the tests"
+if [ $# -eq 1 ]
+	then
+	make
+	START=$(date +%s.%N)
+	pintos --qemu -- -q run $1
+	END=$(date +%s.%N)
+	sleep 2
+	DIFF=$(echo "$END - $START" | bc)
+	echo $DIFF "seconds to run this program"
+else
+	echo "cleaning..."
+	make clean
+	echo "building"
+	make
+	echo "running alarm-signal"
+	cd build
+	pintos --qemu -- -q run alarm-single
+	START=$SECONDS
+	make check
+	END=$SECONDS
+	TIME=`expr $END - $START`
+	echo "it took "$TIME "seconds to run the tests"
+fi
